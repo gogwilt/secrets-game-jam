47caf8515180adb9ceb2ed0a3f56b7b9541efcb5
Cai GoGwilt <cai@ironcladhq.com>
2024-11-20 17:27:50 -0800

    Fix button focus

--- level_select_menu.gd
+++ level_select_menu.gd
@@ -5,13 +5,16 @@ signal level_selected(level_name: String)
 
 const NUM_LEVELS := 4
 
+var next_level_button: Button
+
 func _ready() -> void:
 	save_state.save_data_updated.connect(_update_level_data)
 	_update_level_data()
+	_focus_next_level()
 	
 func _update_level_data() -> void:
+	next_level_button = $Level1
 	var next_level_idx: int
-	var next_level_button: Button
 	for i in range(1, NUM_LEVELS + 1):
 		var level_data = save_state.get_level_data("level_" + str(i))
 		var level = find_child("Level" + str(i), false)
@@ -28,7 +31,8 @@ func _update_level_data() -> void:
 		var level = find_child("Level" + str(i), false)
 		level.find_child("CardBack", false).visible = true
 		level.disabled = true
-			
+
+func _focus_next_level() -> void:
 	if next_level_button:
 		next_level_button.grab_focus()
 	else:
@@ -52,3 +56,8 @@ func _on_level_3_pressed() -> void:
 func _on_level_4_pressed() -> void:
 	#level_selected.emit("level_4")
 	pass
+
+
+func _on_visibility_changed() -> void:
+	if visible:
+		_focus_next_level()
