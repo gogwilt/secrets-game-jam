a2995eddec72ee72117cfef3610abce7dba9a3ff
Cai GoGwilt <cai@ironcladhq.com>
2024-11-27 08:23:39 -0800

    Background music

--- main.gd
+++ main.gd
@@ -6,12 +6,23 @@ var level_player: LevelPlayer
 var is_paused: bool = false
 var current_level_name: String
 var player: Player
-var currently_playing_level: bool = false
+var currently_playing_level: bool = false:
+	set(value):
+		currently_playing_level = value
+		currently_playing_level_updated.emit()
+var currently_in_level: bool = false:
+	set(value):
+		currently_in_level = value
+		currently_in_level_updated.emit()
+		
+signal currently_playing_level_updated
+signal currently_in_level_updated
 
 func _ready() -> void:
 	%CluedomancerSaveState.load_game()
 	$LevelCompleteMenu.visible = false
 	$PauseMenu.visible = false
+	%MenuMusic.play()
 
 func _process(_delta: float) -> void:
 	if not $LevelSelectMenu.visible and currently_playing_level and Input.is_action_just_pressed("pause"):
@@ -49,8 +60,10 @@ func load_and_reset_level(level_name: String) -> void:
 		remove_child(level_player)
 	level_player = level_player_scene.instantiate()
 	add_child(level_player)
+	_disconnect_audio_to_player()
 	player = get_tree().get_first_node_in_group("player")
 	player.can_move = true
+	_connect_audio_to_player()
 	var level_info = %CluedomancerSaveState.get_level_data(current_level_name)
 	level_player.load_level(level_name, level_info)
 	current_level_name = level_name
@@ -59,6 +72,7 @@ func load_and_reset_level(level_name: String) -> void:
 	$LevelSelectMenu.visible = false
 	%DialogueBackground.visible = false
 	currently_playing_level = true
+	currently_in_level = true
 
 	# Connect once at the end, so that _on_level_completed doesn't accidentally fire	
 	level_player.connect("level_completed", _on_level_completed, ConnectFlags.CONNECT_ONE_SHOT)
@@ -71,6 +85,7 @@ func go_to_main_menu() -> void:
 	$PauseMenu.visible = false
 	$LevelSelectMenu.visible = true
 	currently_playing_level = false
+	currently_in_level = false
 
 func _on_level_completed(_level_name, time_elapsed, collected_cards) -> void:
 	player.can_move = false
@@ -131,3 +146,40 @@ func _on_retry_button_pressed() -> void:
 
 func _on_back_to_level_select_button_pressed() -> void:
 	go_to_main_menu()
+
+func _connect_audio_to_player() -> void:
+	player.connect("dimension_switch_status_updated", _on_dimension_switch)
+	
+func _disconnect_audio_to_player() -> void:
+	if not player:
+		return
+	player.disconnect("dimension_switch_status_updated", _on_dimension_switch)
+	
+func _on_dimension_switch(status: Player.DimensionSwitchStatus) -> void:
+	var level_music_bus = AudioServer.get_bus_index("LevelMusic")
+	if status == Player.DimensionSwitchStatus.MAIN_DIMENSION or status == Player.DimensionSwitchStatus.UNABLE_TO_SWITCH_TO_SECRET:
+		AudioServer.set_bus_effect_enabled(level_music_bus, 0, false)
+	else:
+		AudioServer.set_bus_effect_enabled(level_music_bus, 0, true)
+
+
+func _on_currently_in_level_updated() -> void:
+	var level_music_bus = AudioServer.get_bus_index("LevelMusic")
+	if currently_playing_level:
+		# Play level music
+		%LevelMusic.volume_db = 0.0
+		%LevelMusic.play()
+		# Fade out menu music
+		var fade_menu = get_tree().create_tween()
+		fade_menu.tween_property(%MenuMusic, "volume_db", -72.0, 1).set_ease(Tween.EASE_IN)
+		await fade_menu.finished
+		%MenuMusic.stop()
+	else:
+		# Play menu music
+		%MenuMusic.volume_db = 0.0
+		%MenuMusic.play()		
+		# Fade out level music
+		var fade_tween = get_tree().create_tween()
+		fade_tween.tween_property(%LevelMusic, "volume_db", -72.0, 3).set_ease(Tween.EASE_IN)
+		await fade_tween.finished
+		%LevelMusic.stop()
